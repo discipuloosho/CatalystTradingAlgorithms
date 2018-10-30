1c1940552e997a029b2009c2dadcdc797ca7b93b
DonAlguien <ajmtrz@gmail.com>
2018-10-30 01:28:12 +0100

    Simple asset calculator with which to operate according to entry price and stop loss price.

---
+++ MoneyManagement/Manual.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def userInput():
+    while True:
+        try:
+            totalCapital = float(input("Capital total: "))
+            percentageToRisk = float(input("Riesgo en %: "))
+            entryPrice = float(input("Precio de entrada: "))
+            stopLossPrice = float(input("Stop Loss: "))
+            break
+        except ValueError:
+            pass
+    tradeParams = [totalCapital, percentageToRisk, entryPrice, stopLossPrice]
+    return tradeParams
+
+def assetsCalc(tradeParams):
+    capitalToRisk = tradeParams[0] * (tradeParams[1]/100)
+    lostByAssetUntilStopLoss = abs(tradeParams[2] - tradeParams[3])
+    assetsToBuy = capitalToRisk / lostByAssetUntilStopLoss
+    return assetsToBuy
+
+if __name__ == '__main__':
+    tradeParams = userInput()
+    print(f'Total de activos: {assetsCalc(tradeParams)}')
